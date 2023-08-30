a9e65022e606f3ab4362b4bce6d0bcdbfc40687e
nhhasib <114354524+nhhasib@users.noreply.github.com>
2023-08-30 10:13:17 +0000

    error page create

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Navbar from '../component/shared/NavBar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../component/shared/Footer';
+import Navbar from "../component/shared/Navbar"
 
 const Main = () => {
     return (
--- src/component/Pages/Error/Error.jsx
+++ src/component/Pages/Error/Error.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import img from '../../../../public/error.jpg'
+import error from '../../../assets/error.json'
 import { Link } from 'react-router-dom';
+import Lottie from 'react-lottie';
 
 const Error = () => {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: error,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
     return (
         <div className='w-3/5 mx-auto text-center'>
-            <img src={img} alt="" />
+            <Lottie options={defaultOptions}></Lottie>
             <Link to={'/'}><button className='button-primary text-center'>Back to Home</button></Link>
         </div>
     );
